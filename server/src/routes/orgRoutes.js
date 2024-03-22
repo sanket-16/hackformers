844246b53388137775e8f5899ca26b32c612be4f
Bhushan <bhushanpawar9001@gmail.com>
2024-03-23 02:18:21 +0530

    organization incompleto

--- server/src/routes/orgRoutes.js
+++ server/src/routes/orgRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const {  orgController} = require('./../controller/orgController');
+const {  orgController, organizationController} = require('./../controller/orgController');
 const passport = require('passport');
 // const upload = require('./../middlewares/multer')
 
@@ -9,11 +9,16 @@ const router = express.Router();
 //user auth routes
 router.post("/signup", orgController.signup);
 router.post("/signin", orgController.signin);
+router.get("/:id",orgController.getOrg);
 
 //testing route
 router.get("/protected", passport.authenticate('jwt', { session: false }),(req,res)=>{
     res.send(req.user)
 })
 
+//Organization Routes
+
+router.post("/createOrg",passport.authenticate('jwt', { session: false }),organizationController.createOrganization);
+router.post("/addOrganizer/:organizationId",passport.authenticate('jwt', { session: false }),organizationController.addOrganizerToOrganization)
 
 module.exports = router;
